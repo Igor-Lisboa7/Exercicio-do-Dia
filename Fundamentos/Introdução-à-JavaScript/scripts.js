e16a958d9c7862a79fbecb5a26c8811bad032dc8
Igor  Lisboa <igorresendelisboa@gmail.com>
2023-02-27 18:53:19 -0300

    Exemplo if else switch

--- Fundamentos/Introdução-à-JavaScript/scripts.js
+++ Fundamentos/Introdução-à-JavaScript/scripts.js
@@ -1,12 +1,29 @@
-let num1 = 30
-let num2 = 33
+let diaper
+let babySize = 59
 
-if (num1 > num2) {
-    console.log("num1 Wins")
-} 
-else if(num1 == num2){
-    console.log("Empate")
+if(babySize <= 4){
+    diaper = "RN"
 }
-else{
-    console.log("num2 Wins")
+else if (babySize > 4 && babySize <= 6){
+    diaper = "RN+"
 }
+else if(babySize > 6 && babySize <= 8){
+    diaper = "P"
+}
+else if(babySize > 8 && babySize <= 10){
+    diaper = "M"
+}
+else if(babySize > 10 && babySize <= 13){
+    diaper = "G"
+}
+else if(babySize > 13 && babySize <= 15){
+    diaper = "XG"
+}
+else if(babySize > 15  && babySize <= 20){
+    diaper = "XXG"
+}
+else {
+    diaper = "Já está bom, é melhor você utilisar vaso"
+}
+
+console.log(diaper)
