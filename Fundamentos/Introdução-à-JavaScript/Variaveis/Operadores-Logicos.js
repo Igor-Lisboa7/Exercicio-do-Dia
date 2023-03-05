f9b7ffd634fa4f21b7bf146f71283a300c9f21a9
Igor  Lisboa <igorresendelisboa@gmail.com>
2023-03-05 17:23:08 -0300

    Atualização introdução a JavaScript 4.1

---
+++ Fundamentos/Introdução-à-JavaScript/Variaveis/Operadores-Logicos.js
@@ -0,0 +1,19 @@
+currentHour = 23
+message = ""
+
+if(currentHour >= 22){
+    message = "Não deveríamos comer nada, é hora de dormir."
+}
+else if(currentHour >= 18 && currentHour < 22){
+    message = "Rango da noite, vamos jantar."
+}
+else if(currentHour >= 14 && currentHour < 18){
+    message = "Vamos fazer um Bolo para o café da tarde?"
+}
+else if(currentHour >= 11 && currentHour < 14){
+    message = "Hora do almoço!!!"
+} else if(currentHour >= 4 && currentHour < 11){
+    message = "Hmmm! Cheirinho de café recém-passado!"
+}
+
+console.log(message);
