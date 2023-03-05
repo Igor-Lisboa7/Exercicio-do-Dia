ded01b80b7b0ca5865580bb439bab279a02a06c7
Igor  Lisboa <igorresendelisboa@gmail.com>
2023-03-05 16:45:21 -0300

    Atualização introdução a JavaScript 3

---
+++ Fundamentos/Introdução-à-JavaScript/Variaveis/If-Else.js
@@ -0,0 +1,12 @@
+let notaPessoaCandidata =100
+
+if( notaPessoaCandidata >= 80){
+    console.log("Parabéns, você faz parte do grupo de pessoas Aprovadas.")
+}
+else if(notaPessoaCandidata < 80 && notaPessoaCandidata >= 60){
+    console.log("Você está em nossa lista de Espera.")
+}
+else(notaPessoaCandidata < 60) 
+{
+    console.log("Infelizmente, você reprovou.")
+}
