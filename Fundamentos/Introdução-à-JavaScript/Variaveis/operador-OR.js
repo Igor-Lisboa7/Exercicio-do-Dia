ba2aab1a448ca39699647d6375549410336fb998
Igor  Lisboa <igorresendelisboa@gmail.com>
2023-03-06 01:53:55 -0300

    Atualização introdução a JavaScript 4.2

---
+++ Fundamentos/Introdução-à-JavaScript/Variaveis/operador-OR.js
@@ -0,0 +1,8 @@
+ let weekDay = "Quarta-feira"
+ if(weekDay = "Segunda-feira" || "Terça-feira" || "Quarta-feitra" || "Quinta-feira" || "Sexta-feira"){
+    console.log("Oba, mais um dia de aprendizado na Trybe.")
+ }
+ else
+ {
+    console.log()
+ }
