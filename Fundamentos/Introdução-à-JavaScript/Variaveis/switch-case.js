50e973cb9070bc5ff2e00d5d1669580ec1979b5a
Igor  Lisboa <igorresendelisboa@gmail.com>
2023-03-06 02:53:08 -0300

    Atualização introdução a JavaScript 4.3

---
+++ Fundamentos/Introdução-à-JavaScript/Variaveis/switch-case.js
@@ -0,0 +1,19 @@
+let status = "aprovada"
+
+switch(status){
+    case "aprovada":
+        console.log("Parabéns, você está no grupo de pessoas aprovadas!");
+        break;
+    
+    case "lista":
+        console.log("Você está na nossa lista de espera.");
+        break;
+    
+    case "reprovada":
+        console.log("Infelizmente, você repovou.");
+        break;
+
+    default:
+        console.log("Informação incorreta!")
+
+    }
